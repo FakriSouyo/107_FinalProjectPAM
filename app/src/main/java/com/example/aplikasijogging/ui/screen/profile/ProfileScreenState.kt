acc9bbf2422c9e5d24e2865ebc0aa0d955cf7eca
FakriSouyo <fakriggwp@gmail.com>
2024-01-12 05:38:39 +0700

    profile

---
+++ app/src/main/java/com/example/aplikasijogging/ui/screen/profile/ProfileScreenState.kt
@@ -0,0 +1,13 @@
+package com.example.aplikasijogging.ui.screen.profile
+
+import com.example.aplikasijogging.data.model.User
+
+
+data class ProfileScreenState(
+    val totalDistanceInKm: Float = 0f,
+    val totalDurationInHr: Float = 0f,
+    val totalCaloriesBurnt: Long = 0,
+    val user: User = User(),
+    val isEditMode: Boolean = false,
+    val errorMsg: String? = null
+)
